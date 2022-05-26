9390e541be9592e000b1e95f0c2adc886d7a5052
aliakbarrehman <aliakbarrehman@gmail.com>
2022-05-26 14:16:50 +0200

    Installing missing build tool

--- spinUpJupyterhub.sh
+++ spinUpJupyterhub.sh
@@ -2,6 +2,8 @@
 ROOT_DIRECTORY=${PWD}
 REPOSITORY_NAME="aliakbarrehman/jupyterhub"
 
+
+pip install build
 echo "Building Jupyterhub with Repository $REPOSITORY_NAME and Running Jupyterhub"
 cd $ROOT_DIRECTORY/jupyterhub && ./buildJupyterhub.sh $REPOSITORY_NAME  &
 wait && cd $ROOT_DIRECTORY/jupyterhub && ./runJupyterhub.sh jhub config.yaml
